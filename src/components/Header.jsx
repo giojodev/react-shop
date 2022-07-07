3a892a7e663021727e916761394f7fa6999d6f69
Giovanni Orozco <whoisyio@gmail.com>
2022-07-06 23:36:33 -0600

    Cambios al header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -39,7 +39,7 @@ const Header = () => {
 			</div>
 			<div className="navbar-right">
 				<ul>
-					<li className="navbar-email" onClick={handleToggle}>
+					<li className="navbar-email ejemplogio" onClick={handleToggle}>
 						platzi@example.com
 					</li>
 					<li className="navbar-shopping-cart">
